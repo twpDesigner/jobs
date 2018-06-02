5b50bb517de9cc9a86bb84a3563e6b4bba81e90c
Doris颖 <dorisying@DorisyingdeiMac.local>
2018-06-02 11:15:02 +0800

    ss

--- src/main/java/com/xwtec/jobs/Application.java
+++ src/main/java/com/xwtec/jobs/Application.java
@@ -3,7 +3,6 @@ package com.xwtec.jobs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 @EnableScheduling
 @SpringBootApplication
 public class Application {
